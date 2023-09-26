57a3d6f9ca3cf6afbf5b927bda56c9b04caa81a1
Yonashenok <yonashenok24@gmail.com>
2023-09-26 15:51:57 +0300

    feat: assocation with user, comments, and likes

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+  # Association
+  belongs_to :user, class_name: 'User', foreign_key: 'author_id'
+  has_many :comments, class_name: 'Comment'
+  has_many :likes, class_name: 'Like'
+  def update_post_count
+    user.update(post_counter: user.posts.count)
+  end
+
+  # methods
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end

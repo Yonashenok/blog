4585800ad8fcbd4e9f788fc285181df791831414
Yonashenok <yonashenok24@gmail.com>
2023-10-02 16:30:57 +0300

    fix: linter error

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,22 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
-  
   describe 'GET /users/:user_id/posts' do
-    let(:user) { User.create(name: 'Luna', photo: 'https://picsum.photos/id/64/200', bio: 'Teacher from Mexico.', post_counter: 2)  } 
-   
+    let(:user) do
+      User.create(name: 'Luna', photo: 'https://picsum.photos/id/64/200', bio: 'Teacher from Mexico.', post_counter: 2)
+    end
+
     it 'returns a success response' do
-      get user_posts_path(user)  
+      get user_posts_path(user)
       expect(response).to have_http_status(:success)
     end
-    it "renders the index template" do
+    it 'renders the index template' do
       get user_posts_path(user)
-      expect(response).to render_template("index")
+      expect(response).to render_template('index')
     end
   end
-  
+
   describe 'GET /users/:user_id/posts/:id' do
-    let(:user) { User.create(name: 'Luna', photo: 'https://picsum.photos/id/64/200', bio: 'Teacher from Mexico.', post_counter: 2)  } 
+    let(:user) do
+      User.create(name: 'Luna', photo: 'https://picsum.photos/id/64/200', bio: 'Teacher from Mexico.', post_counter: 2)
+    end
     let(:post) do
       user.posts.create(title: 'Sample Post', text: 'My new post', comments_counter: 1, likes_counter: 1)
     end
@@ -24,10 +27,15 @@ RSpec.describe 'Posts', type: :request do
       get user_post_path(user, post)
       expect(response).to have_http_status(:success)
     end
-   
+
     it 'renders the show template' do
       get user_post_path(user, post)
       expect(response).to render_template(:show)
+    end
+
+    it 'includes correct placeholder text in the response body' do
+      get user_post_path(user, post)
+      expect(response.body).to include('This post details for a user')
     end
   end
 end
--- spec/requests/user_spec.rb
+++ spec/requests/user_spec.rb
@@ -6,22 +6,29 @@ RSpec.describe 'Users', type: :request do
       get users_path
       expect(response).to have_http_status(:success)
     end
-    it "renders the index template" do
+    it 'renders the index template' do
       get users_path
-      expect(response).to render_template("index")
+      expect(response).to render_template('index')
     end
   end
 
   describe 'GET /users/:id' do
-    let(:user) { User.create(name: 'Luna', photo: 'https://picsum.photos/id/64/200', bio: 'Teacher from Mexico.', post_counter: 2)  } 
+    let(:user) do
+      User.create(name: 'Luna', photo: 'https://picsum.photos/id/64/200', bio: 'Teacher from Mexico.', post_counter: 2)
+    end
     it 'returns a success response' do
       get user_path(user)
       expect(response).to have_http_status(:success)
     end
-   
+
     it 'renders the show template' do
       get user_path(user)
-      expect(response).to render_template("show")
+      expect(response).to render_template('show')
+    end
+
+    it 'includes correct placeholder text in the response body' do
+      get user_path(user)
+      expect(response.body).to include('This is user detail')
     end
   end
 end

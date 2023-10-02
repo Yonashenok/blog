8011601250e78b2545c58b9aff5763fc6b6aba8f
Yonashenok <yonashenok24@gmail.com>
2023-10-02 16:16:01 +0300

    test: post controller

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,7 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  
+  describe 'GET /users/:user_id/posts' do
+    let(:user) { User.create(name: 'Luna', photo: 'https://picsum.photos/id/64/200', bio: 'Teacher from Mexico.', post_counter: 2)  } 
+   
+    it 'returns a success response' do
+      get user_posts_path(user)  
+      expect(response).to have_http_status(:success)
+    end
+    it "renders the index template" do
+      get user_posts_path(user)
+      expect(response).to render_template("index")
+    end
+  end
+  
+  describe 'GET /users/:user_id/posts/:id' do
+    let(:user) { User.create(name: 'Luna', photo: 'https://picsum.photos/id/64/200', bio: 'Teacher from Mexico.', post_counter: 2)  } 
+    let(:post) do
+      user.posts.create(title: 'Sample Post', text: 'My new post', comments_counter: 1, likes_counter: 1)
+    end
+    it 'returns a success response' do
+      get user_post_path(user, post)
+      expect(response).to have_http_status(:success)
+    end
+   
+    it 'renders the show template' do
+      get user_post_path(user, post)
+      expect(response).to render_template(:show)
+    end
   end
 end

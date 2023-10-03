4b5dae633a53e6a2a7b0a4671322a3617ccd05f8
Yonashenok <yonashenok24@gmail.com>
2023-10-03 17:04:22 +0300

    fix: linter error

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,14 +1,14 @@
 class LikesController < ApplicationController
-    def create
-        @post = Post.find(params[:post_id])
-        @like = Like.new(user: current_user, post: @post)
-    
-        if @like.save
-        flash[:notice] = 'You liked this post!'
-        redirect_to user_posts_path
-        else 
-            flash[:alert] = 'Something went wrong!'
-            render :new
-        end
+  def create
+    @post = Post.find(params[:post_id])
+    @like = Like.new(user: current_user, post: @post)
+
+    if @like.save
+      flash[:notice] = 'You liked this post!'
+      redirect_to user_posts_path
+    else
+      flash[:alert] = 'Something went wrong!'
+      render :new
     end
+  end
 end

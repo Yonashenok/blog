09148735b24ae607495bce136359ada6192c9f31
Yonashenok <yonashenok24@gmail.com>
2023-10-03 15:29:21 +0300

    feat: form for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,27 +11,13 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
-  def create
+  def create 
     @post = current_user.posts.build(params.require(:post).permit(:title, :text))
-    if @post.save
-      redirect_to user_path
-    else
+    if  @post.save
+      flash[:notice] = "Post was created seccesufuly"
+      redirect_to user_path(id: current_user)
+    else 
       render :new
     end
   end
-
-  # def new 
-  #   @post =current_user.posts.build
-  #   @user = User.find(params[:user_id])
-  # end
-
-  # def create 
-  #   @post = current_user.posts.build(params.require(:post).permit(:title, :text))
-  #   if  @post.save
-  #     flash[:notice] = "Post was created seccesufuly"
-  #     redirect_to user_path(id: current_user)
-  #   else 
-  #     render :new
-  #   end
-  # end
 end

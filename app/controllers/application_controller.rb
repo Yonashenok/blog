7765c276e46085e6a1598a2f36ce614a17a313c6
Yonashenok <yonashenok24@gmail.com>
2023-10-11 12:54:57 +0300

    feat: update on controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,10 +6,5 @@ class ApplicationController < ActionController::Base
 
   protected
 
-  def update_allowed_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :bio, :password, :photo) }
-    devise_parameter_sanitizer.permit(:account_update) do |u|
-      u.permit(:name, :surname, :email, :bio, :password, :current_password, :photo)
-    end
-  end
+
 end
